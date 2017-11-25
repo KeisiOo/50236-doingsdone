dc90ef3530ae753a5e83d54230c828fd46cd1523
KeisiOo <jamesnsk90@yandex.ru>
2017-11-25 12:07:43 +0700

    Шаблоны 1.1

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 require_once ('functions.php');
 require_once ('data.php');
 
-$pageContent = includeTemplate('templates/index.php', ['tasks' => $tasks]);
+$pageContent = includeTemplate('templates/index.php', [
+	'tasks' => $tasks,
+	'show_complete_tasks' => $show_complete_tasks,
+	'days_until_deadline' => $days_until_deadline
+]);
+
 $layoutContent = includeTemplate('templates/layout.php', [
 	'content' => $pageContent,
 	'projects' => $projects,
--- templates/index.php
+++ templates/index.php
@@ -1,20 +1,3 @@
- <?php
-// показывать или нет выполненные задачи
-$show_complete_tasks = rand(0, 1);
-// устанавливаем часовой пояс в Московское время
-date_default_timezone_set('Europe/Moscow');
-
-$days = rand(-3, 3);
-$task_deadline_ts = strtotime("+" . $days . " day midnight"); // метка времени даты выполнения задачи
-$current_ts = strtotime('now midnight'); // текущая метка времени
-
-// запишите сюда дату выполнения задачи в формате дд.мм.гггг
-$date_deadline = date("d.m.Y", $task_deadline_ts);
-
-// в эту переменную запишите кол-во дней до даты задачи
-$days_until_deadline = floor(($task_deadline_ts - $current_ts) / 86400);
-?>
-
 <h2 class="content__main-heading">Список задач</h2>
 
 <form class="search-form" action="index.html" method="post">
